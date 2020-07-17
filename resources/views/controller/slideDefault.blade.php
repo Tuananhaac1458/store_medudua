2fe8b0e4017ced585ec448b8a9282ab840dfc90f
Arius <ariuslee123@gmail.com>
2020-07-18 02:24:19 +0700

    update 17/08/2020
    Signed-off-by: Arius <ariuslee123@gmail.com>

--- resources/views/controller/slideDefault.blade.php
+++ resources/views/controller/slideDefault.blade.php
@@ -2,9 +2,12 @@
             <div class="carousel-inner ">
                 <div class="carousel-item active">
                     <div class="row " >
-                       
                         <div class="col-lg-6 col-sm-12 no-pd" >
-                            <div class="d-none d-md-block">
+                            <img class="d-block w-100 block_text_slide" src="https://i.pinimg.com/originals/e8/22/7d/e8227d3d1a6cf1d72762e5cd22ea1bd2.jpg" alt="First slide">
+                        </div>
+
+                        <div class="col-lg-6 col-sm-12 no-pd" >
+                            <div class="d-md-block">
                                 <div class="block_text_slide">
                                     <h2 style="color: black;">With cute packaging and tongue-and-cheek marketing, they've become a huge Aussie success story.</h2>
                                     <div class="div_buttom_01">
@@ -16,18 +19,16 @@
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-sm-12 no-pd" >
-                            <img class="d-block w-100" src="https://i.pinimg.com/originals/e8/22/7d/e8227d3d1a6cf1d72762e5cd22ea1bd2.jpg" alt="First slide">
-                        </div>
+                        
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="row " >
                         <div class="col-lg-6 col-sm-12 no-pd" >
-                            <img class="d-block w-100" src="https://i.pinimg.com/originals/e8/22/7d/e8227d3d1a6cf1d72762e5cd22ea1bd2.jpg" alt="First slide">
+                            <img class="d-block w-100 block_text_slide" src="https://i.pinimg.com/originals/e8/22/7d/e8227d3d1a6cf1d72762e5cd22ea1bd2.jpg" alt="First slide">
                         </div>
                         <div class="col-lg-6 col-sm-12 no-pd" >
-                            <div class="d-none d-md-block">
+                            <div class="d-md-block">
                                 <h1 style="color: black;">aaaaaaaaaaa</h1>
                             </div>
                         </div>
@@ -36,10 +37,10 @@
                 <div class="carousel-item">
                     <div class="row " >
                         <div class="col-lg-6 col-sm-12 no-pd" >
-                            <img class="d-block w-100" src="https://image.winudf.com/v2/image/Z3ltLmdpcmxzLndhbGxwYXBlci5od19zY3JlZW5zaG90c18xX2E3N2QzMDky/screen-1.jpg?fakeurl=1&type=.jpg" alt="Third slide">
+                            <img class="d-block w-100 block_text_slide" src="https://image.winudf.com/v2/image/Z3ltLmdpcmxzLndhbGxwYXBlci5od19zY3JlZW5zaG90c18xX2E3N2QzMDky/screen-1.jpg?fakeurl=1&type=.jpg" alt="Third slide">
                         </div>
                         <div class="col-lg-6 col-sm-12 no-pd" >
-                            <div class="d-none d-md-block">
+                            <div class="d-md-block">
                                 <h1 style="color: black;">aaaaaaaaaaa</h1>
                             </div>
                         </div>
